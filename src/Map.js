bc120f9788a2ff50855fc5d8a585b34a6e78bce7
Yishin-Jheng <lf90691@gmail.com>
2022-12-08 16:41:26 +0800

    Add the part related to Leaflet map

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,23 @@
-function Map() {
-  return <div className="map">Map</div>;
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+function Map({ locData }) {
+  const position = locData;
+
+  return (
+    <MapContainer
+      className="map"
+      center={position}
+      zoom={8}
+      scrollWheelZoom={true}
+      key={`${locData}`}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+      />
+      <Marker position={position}></Marker>
+    </MapContainer>
+  );
 }
 
 export default Map;
