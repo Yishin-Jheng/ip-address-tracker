73fb66cecf42b6068b628c70fd9b16f6d88e94b9
yishin <lf90691@gmail.com>
2022-12-15 00:17:29 +0800

    Change the marker icon in the map and update readme file

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,16 @@
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import marker from "./images/icon-location.png";
 
 function Map({ locData }) {
   const position = locData;
+
+  const customIcon = new L.Icon({
+    iconUrl: marker,
+    iconRetinaUrl: marker,
+    iconSize: [30, 37],
+    iconAnchor: [15, 37],
+  });
 
   return (
     <MapContainer
@@ -15,7 +24,7 @@ function Map({ locData }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
       />
-      <Marker position={position}></Marker>
+      <Marker position={position} icon={customIcon}></Marker>
     </MapContainer>
   );
 }
